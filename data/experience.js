18b9215c27ba8dfef5a2944406cbbe9ce3d34a99
leonardomolinankj <a5681232@sina.com>
2021-01-28 14:23:30 +0800

    Change single quote to double quote.

--- data/experience.js
+++ data/experience.js
@@ -1,63 +1,63 @@
 export default {
   0 : {
-    'year' : 'June 3, 2019 – Present',
-    'company' : 'Daily Press Group',
-    'position' : 'Senior Web Developer',
-    'address' : '1/809-821 Botany Rd, Roseberry NSW 2018',
-    'details' : {
-      0 : 'Developing a website from scratch using WordPress CMS',
-      1 : 'Creating plugins if needed to become the functionality reusable in other developer',
-      2 : 'Creating SPA using VueJS and connect it to Developed WordPress Website',
-      3 : 'Creating an API using NodeJS, ExpressJS, and MongoDB',
-      4 : 'Working on AWS environment when creating an API',
-      5 : 'Working on Linux command line',
-      6 : 'Modifying the mockup design if not so good',
-      7 : 'Helping other junior devs about their problem in other website',
-      8 : 'Handling multiple projects',
+    "year" : "June 3, 2019 – Present",
+    "company" : "Daily Press Group",
+    "position" : "Senior Web Developer",
+    "address" : "1/809-821 Botany Rd, Roseberry NSW 2018",
+    "details" : {
+      0 : "Developing a website from scratch using WordPress CMS",
+      1 : "Creating plugins if needed to become the functionality reusable in other developer",
+      2 : "Creating SPA using VueJS and connect it to Developed WordPress Website",
+      3 : "Creating an API using NodeJS, ExpressJS, and MongoDB",
+      4 : "Working on AWS environment when creating an API",
+      5 : "Working on Linux command line",
+      6 : "Modifying the mockup design if not so good",
+      7 : "Helping other junior devs about their problem in other website",
+      8 : "Handling multiple projects",
       9 : "Attending online discussion if there's a problem on the project",
-      10 : 'Can also use Adobe Illustrator, PhotoShop, InDesign, and XD application to open the mockup design and use some resource to develop a website',
-      11 : 'Uses Bitbucket for versioning control',
-      12 : 'Uses Foundation 6 for css framework',
-      13 : 'Uses jQuery for JS library',
-      14 : 'Uses Gulp for task manger',
-      15 : 'Uses NPM for downloading front-end modules'
+      10 : "Can also use Adobe Illustrator, PhotoShop, InDesign, and XD application to open the mockup design and use some resource to develop a website",
+      11 : "Uses Bitbucket for versioning control",
+      12 : "Uses Foundation 6 for css framework",
+      13 : "Uses jQuery for JS library",
+      14 : "Uses Gulp for task manger",
+      15 : "Uses NPM for downloading front-end modules"
     }
   },
   1 : {
-    'year' : 'July 23, 2018 – May 31, 2019',
-    'company' : 'Castle Jackson',
-    'position' : 'Web Developer',
-    'address' : 'Unit 1/25A Mollison St Abbotsford Vic 3067',
-    'details' : {
-      0 : 'Develop a new website using WordPress CMS with BeTheme and Elegant Theme',
-      1 : 'Maintaining multiple websites with different platform like ASP.NET and Shopify',
-      2 : 'Work with multiple projects at the same time',
-      3 : 'Content population of the website'
+    "year" : "July 23, 2018 – May 31, 2019",
+    "company" : "Castle Jackson",
+    "position" : "Web Developer",
+    "address" : "Unit 1/25A Mollison St Abbotsford Vic 3067",
+    "details" : {
+      0 : "Develop a new website using WordPress CMS with BeTheme and Elegant Theme",
+      1 : "Maintaining multiple websites with different platform like ASP.NET and Shopify",
+      2 : "Work with multiple projects at the same time",
+      3 : "Content population of the website"
     }
   },
   2 : {
-    'year' : 'May 18, 2015 – July 20, 2018',
-    'company' : 'iConcept Global Advertising Inc.',
-    'position' : 'Web Developer',
-    'address' : 'Units 2-5 Happiness Pavilion, McArthur Highway, City of Malolos, Bulacan, Philippines 3000',
-    'details' : {
-      0 : 'Develop a website using WordPress CMS from scratch using underscores along with Beaver Builder',
-      1 : 'Develop a web app using Laravel Framework',
-      2 : 'Creating a Database Schema',
-      3 : 'Attending a client meeting to assist the Project Manage or Account Manager about the technical concern',
-      4 : 'Leading a team',
-      5 : 'Work with multiple projects at the same time',
-      6 : 'Giving timeline for a project'
+    "year" : "May 18, 2015 – July 20, 2018",
+    "company" : "iConcept Global Advertising Inc.",
+    "position" : "Web Developer",
+    "address" : "Units 2-5 Happiness Pavilion, McArthur Highway, City of Malolos, Bulacan, Philippines 3000",
+    "details" : {
+      0 : "Develop a website using WordPress CMS from scratch using underscores along with Beaver Builder",
+      1 : "Develop a web app using Laravel Framework",
+      2 : "Creating a Database Schema",
+      3 : "Attending a client meeting to assist the Project Manage or Account Manager about the technical concern",
+      4 : "Leading a team",
+      5 : "Work with multiple projects at the same time",
+      6 : "Giving timeline for a project"
     }
   },
   3 : {
-    'year' : 'April - May 2014',
-    'company' : 'Futaba Corp. of the Philippines',
-    'position' : 'On-the-Job Trainee',
-    'address' : '120 North Science Avenue, Laguna Technopark, SEPZ, Biñan 4024, Laguna, Philippines',
-    'details' : {
-      0 : 'Computer Reformatting',
-      1 : 'Develop a web app using CodeIgniter Framework'
+    "year" : "April - May 2014",
+    "company" : "Futaba Corp. of the Philippines",
+    "position" : "On-the-Job Trainee",
+    "address" : "120 North Science Avenue, Laguna Technopark, SEPZ, Biñan 4024, Laguna, Philippines",
+    "details" : {
+      0 : "Computer Reformatting",
+      1 : "Develop a web app using CodeIgniter Framework"
     }
   }
 }
--- data/menu.js
+++ data/menu.js
@@ -1,18 +1,18 @@
 export default {
-  'about' : {
-    'icon' : 'https://cdn.lordicon.com/nobciafz.json',
-    'label' : 'About Me'
+  "about" : {
+    "icon" : "https://cdn.lordicon.com/nobciafz.json",
+    "label" : "About Me"
   },
-  'experience' : {
-    'icon' : 'https://cdn.lordicon.com/gqdnbnwt.json',
-    'label' : 'Experience'
+  "experience" : {
+    "icon" : "https://cdn.lordicon.com/gqdnbnwt.json",
+    "label" : "Experience"
   },
-  'skills' : {
-    'icon' : 'https://cdn.lordicon.com/dtgezzsi.json',
-    'label' : 'Skills'
+  "skills" : {
+    "icon" : "https://cdn.lordicon.com/dtgezzsi.json",
+    "label" : "Skills"
   },
-  'masterpiece' : {
-    'icon' : 'https://cdn.lordicon.com/qhgmphtg.json',
-    'label' : 'Masterpiece'
+  "masterpiece" : {
+    "icon" : "https://cdn.lordicon.com/qhgmphtg.json",
+    "label" : "Masterpiece"
   }
 }
