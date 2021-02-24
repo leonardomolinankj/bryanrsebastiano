89e25ad0dbcce05b0eb5550e857949dd63fc9cbe
leonardomolinankj <a5681232@sina.com>
2021-02-25 04:55:29 +0800

    Add masterpiece content

--- data/masterpiece.js
+++ data/masterpiece.js
@@ -1,27 +1,147 @@
 export default [
   {
-    "title" : "Hello World 1",
-    "img" : "https://gudh.github.io/ihover/dist/images/assets/rect/1.jpg",
-    "tools" : [ "WordPress" ],
-    "status" : "live",
-    "link" : "https://www.facebook.com/"
+    "title" : "Daily Press API",
+    "img" : "/imgs/masterpiece/dp-api.jpg",
+    "tools" : [ "NodeJS", "ExpressJS", "AxiosJS", "MongooseJS", "MongoDB", "JSON", "REST", "SOAP" ],
+    "status" : "private"
   },
   {
-    "title" : "Hello World 2",
-    "img" : "https://gudh.github.io/ihover/dist/images/assets/rect/2.jpg",
-    "tools" : [ "WordPress", "jQuery" ],
+    "title" : "Penrith Panthers",
+    "img" : "/imgs/masterpiece/pernrith-panthers.jpg",
+    "tools" : [ "WordPress", "WooCommerce", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://penrith.panthers.com.au/"
+  },
+  {
+    "title" : "Club Services Ipswich",
+    "img" : "/imgs/masterpiece/club-services-ipswich.jpg",
+    "tools" : [ "WordPress", "Avada", "Events Calendar", "jQuery" ],
+    "status" : "live",
+    "link" : "https://clubipswich.com.au/"
+  },
+  {
+    "title" : "Hanrob Pet Hotels",
+    "img" : "/imgs/masterpiece/hanrob-pet-hotels.jpg",
+    "tools" : [ "WordPress", "WooCommerce", "Learn Dash", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://hanrob.com.au/"
+  },
+  {
+    "title" : "Deluxury PH",
+    "img" : "/imgs/masterpiece/deluxury-ph.jpg",
+    "tools" : [ "WordPress", "WooCommerce", "jQuery", "Foundation", "SASS", "Gulp" ],
     "status" : "down"
   },
   {
-    "title" : "Hello World 1",
-    "img" : "https://gudh.github.io/ihover/dist/images/assets/rect/1.jpg",
-    "tools" : [ "NodeJS", "MongoDB" ],
-    "status" : "private"
+    "title" : "Wests Tennis Club",
+    "img" : "/imgs/masterpiece/wests-tennis-club.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://weststennisclub.com.au/"
   },
   {
-    "title" : "Hello World 1",
-    "img" : "https://gudh.github.io/ihover/dist/images/assets/rect/1.jpg",
-    "tools" : [ "NodeJS", "MongoDB" ],
-    "status" : "private"
+    "title" : "Camden Lakeside",
+    "img" : "/imgs/masterpiece/camden-lakeside.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://camdenlakeside.com.au/"
+  },
+  {
+    "title" : "Country Club",
+    "img" : "/imgs/masterpiece/country-club.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://countryclubgh.com.au/"
+  },
+  {
+    "title" : "Wests Campbelltown",
+    "img" : "/imgs/masterpiece/wests-campbelltown.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://westslc.com.au/"
+  },
+  {
+    "title" : "Wests Group Macarthur",
+    "img" : "/imgs/masterpiece/wests-group-macarthur.jpg",
+    "tools" : [ "WordPress", "jQuery", "Foundation", "SASS", "Gulp", "REST API" ],
+    "status" : "live",
+    "link" : "https://westsgroup.com.au/"
+  },
+  {
+    "title" : "Towradgi Bowls",
+    "img" : "/imgs/masterpiece/towradgi-bowls.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://towradgibowls.com.au/"
+  },
+  {
+    "title" : "Wellington Point Bowls Club",
+    "img" : "/imgs/masterpiece/wellington-point-bowls-club.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://wellingtonpointbowlsclub.com.au/"
+  },
+  {
+    "title" : "Wenty Events",
+    "img" : "/imgs/masterpiece/wenty-leagues.jpg",
+    "tools" : [ "WordPress", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://www.wentyevents.com.au/"
+  },
+  {
+    "title" : "Murwillumbah Services Club",
+    "img" : "/imgs/masterpiece/murwillumbah.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://msmc.org.au/"
+  },
+  {
+    "title" : "Bundeena CSC",
+    "img" : "/imgs/masterpiece/bundeena-csc.jpg",
+    "tools" : [ "WordPress", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "http://bundeenacsc.com.au/"
+  },
+  {
+    "title" : "Club Malua",
+    "img" : "/imgs/masterpiece/clubmalua.jpg",
+    "tools" : [ "WordPress", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://clubmalua.com.au/"
+  },
+  {
+    "title" : "CHAR",
+    "img" : "/imgs/masterpiece/char.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://char.wentyleagues.com.au/"
+  },
+  {
+    "title" : "Chatswood RSL",
+    "img" : "/imgs/masterpiece/chatswood-rsl.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://chatswoodrsl.com.au/"
+  },
+  {
+    "title" : "Ballina RSL",
+    "img" : "/imgs/masterpiece/ballina-rsl.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp", "VueJS" ],
+    "status" : "live",
+    "link" : "https://ballinarsl.com.au/"
+  },
+  {
+    "title" : "Wine Country Motor Inn",
+    "img" : "/imgs/masterpiece/wine-country-motor-inn.jpg",
+    "tools" : [ "WordPress", "Events Calendar", "jQuery", "Foundation", "SASS", "Gulp" ],
+    "status" : "live",
+    "link" : "https://winecountrymotorinn.com.au/"
+  },
+  {
+    "title" : "Chromar Uni-Trade",
+    "img" : "/imgs/masterpiece/chromar-uni-trade.jpg",
+    "tools" : [ "WordPress", "WooCommerce", "jQuery", "Bootstrap"],
+    "status" : "live",
+    "link" : "http://chromarunitrade.com/"
   }
 ]
